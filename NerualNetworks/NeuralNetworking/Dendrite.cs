bdaa151d4cba645a2a37d52158f24fa669575427
Mi'Angel <pata.miangel@gmail.com>
2019-04-09 17:46:36 -0700

    Add project files.

---
+++ NerualNetworks/NeuralNetworking/Dendrite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using NerualNetworks.NeuralNetworking;
+
+namespace NeuralNetworks.NeuralNetworking
+{
+    class Dendrite
+    {
+        public Pulse InputPulse { get; set; }
+        public double SynapticWeight { get; set; }
+
+        public bool Learnable { get; set; } = true;
+    }
+}
